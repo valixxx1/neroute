c10c87fdff83fdb927c3383a0aba73bfe1079e0d
vgershun <vgershun@gmail.com>
2025-06-20 19:03:20 +0300

    help menu and version

--- source/neroute.c
+++ source/neroute.c
@@ -1,11 +1,35 @@
 /*  Copyright (c) 2025 valixxx1
- *  This code is licensed under MIT license (see LICENSE.txt for details)
+ *  This code is licensed under MIT license (see LICENSE for details)
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-  puts("neroute");
+  opterr = 0;
+
+  char curopt;
+  while ((curopt = getopt(argc, argv, "hv")) != -1) {
+    switch (curopt) {
+      case 'h':
+        puts("neroute - utility for displaying possible routes and transit delays of IP packets\n\
+Options:\n\
+  -h\n\
+    Print this help menu\n\
+  -v\n\
+    Print version of neroute");
+        exit(0);
+      case 'v':
+        puts("Currently in development");
+        exit(0);
+      case '?':
+        puts("Illegal option");
+        exit(0);
+    }
+  }
+
+  puts("TODO");
   return 0;
 }
