41cc3ad86f87cc4e8488d49f79e0f4ef7821f51b
vgershun <vgershun@gmail.com>
2025-06-20 11:14:15 +0300

    cmake

---
+++ source/neroute.c
@@ -0,0 +1,11 @@
+/*  Copyright (c) 2025 valixxx1
+ *  This code is licensed under MIT license (see LICENSE.txt for details)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+  puts("neroute");
+  return 0;
+}
